ca68f8be934e7ba798900f5d6e47314e325e1e9d
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-06 14:43:22 +0200

    Passwortabfrage

---
+++ Passwortabfrage/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OutputTextBox = New System.Windows.Forms.TextBox()
+        Me.PasswordBox = New System.Windows.Forms.TextBox()
+        Me.OkButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OutputTextBox
+        '
+        Me.OutputTextBox.Enabled = False
+        Me.OutputTextBox.Location = New System.Drawing.Point(12, 12)
+        Me.OutputTextBox.Multiline = True
+        Me.OutputTextBox.Name = "OutputTextBox"
+        Me.OutputTextBox.Size = New System.Drawing.Size(260, 164)
+        Me.OutputTextBox.TabIndex = 0
+        '
+        'PasswordBox
+        '
+        Me.PasswordBox.Location = New System.Drawing.Point(12, 182)
+        Me.PasswordBox.Name = "PasswordBox"
+        Me.PasswordBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9829)
+        Me.PasswordBox.Size = New System.Drawing.Size(260, 20)
+        Me.PasswordBox.TabIndex = 1
+        '
+        'OkButton
+        '
+        Me.OkButton.Location = New System.Drawing.Point(103, 208)
+        Me.OkButton.Name = "OkButton"
+        Me.OkButton.Size = New System.Drawing.Size(75, 23)
+        Me.OkButton.TabIndex = 2
+        Me.OkButton.Text = "OK"
+        Me.OkButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.OkButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 241)
+        Me.Controls.Add(Me.OkButton)
+        Me.Controls.Add(Me.PasswordBox)
+        Me.Controls.Add(Me.OutputTextBox)
+        Me.Name = "Form1"
+        Me.Text = "Passwortabfrage"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OutputTextBox As TextBox
+    Friend WithEvents PasswordBox As TextBox
+    Friend WithEvents OkButton As Button
+End Class
---
+++ Passwortabfrage/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub OkButton_Click(sender As Object, e As EventArgs) Handles OkButton.Click
+
+        Select Case PasswordBox.Text
+            Case "admin"
+                OutputTextBox.Text = "Sie sind als Administrator angemeldet."
+
+            Case "user"
+                OutputTextBox.Text = "Sie sind als User angemeldet."
+
+            Case Else
+                OutputTextBox.Text = "Sie sind als Gast angemeldet."
+
+        End Select
+
+    End Sub
+End Class
