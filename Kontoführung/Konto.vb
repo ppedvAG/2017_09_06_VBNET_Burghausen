0d408a5cfb7a4dcd01ce21b8ad9eafdab0ba0c67
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-07 15:13:30 +0200

    Kontoführung

---
+++ Kontoführung/Konto.vb
@@ -0,0 +1,24 @@
+﻿Public Class Konto
+
+    Private _kontostand As Decimal
+    Public Property Kontostand As Decimal
+        Get
+            Return _kontostand
+        End Get
+        Private Set(value As Decimal)
+            _kontostand = value
+        End Set
+    End Property
+
+    Public Sub New(startkapital As Decimal)
+        _kontostand = startkapital
+    End Sub
+
+    Public Sub Einzahlen(betrag As Decimal)
+        Kontostand += betrag
+    End Sub
+    Public Sub Auszaheln(betrag As Decimal)
+        Kontostand -= betrag
+    End Sub
+
+End Class
---
+++ Kontoführung/Module1.vb
@@ -0,0 +1,40 @@
+﻿Option Strict On
+Imports System.Text
+
+Module Module1
+
+    Sub Main()
+        Console.OutputEncoding = Encoding.UTF8
+
+        Dim girokonto = New Konto(5000)
+
+        While True
+
+            Console.WriteLine()
+            Console.WriteLine($"Aktueller Kontostand: {girokonto.Kontostand}€")
+            Console.Write("(1) Einzahlen | (2) Auszahlen | (0) Beenden : ")
+            Dim eingabe = Console.ReadLine()
+
+            Select Case eingabe
+                Case "0" : Return
+
+                Case "1"
+                    Console.Write("Betrag einzahlen: ")
+                    eingabe = Console.ReadLine()
+                    girokonto.Einzahlen(CDec(eingabe))
+
+                Case "2"
+                    Console.Write("Betrag auszahlen: ")
+                    eingabe = Console.ReadLine()
+                    girokonto.Auszaheln(CDec(eingabe))
+
+                Case Else
+                    Console.WriteLine("Ungültige Eingabe.")
+
+            End Select
+
+        End While
+
+    End Sub
+
+End Module
