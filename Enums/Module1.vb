0de782995dc561c50e1c50fe825e8185bc364dd8
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-08 14:41:49 +0200

    Enumerationen

---
+++ Enums/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim day = GetDay()
+
+
+        Dim key = Console.ReadKey().Key
+
+        Console.ForegroundColor = ConsoleColor.DarkMagenta
+
+
+        Select Case (day)
+            Case Wochentag.Montag
+
+            Case Wochentag.Dienstag
+        End Select
+
+    End Sub
+
+    Public Function GetDay() As Wochentag
+        'irgend eine komplexe Berechnung....
+        Return Wochentag.Freitag
+    End Function
+
+End Module
---
+++ Enums/Wochentag.vb
@@ -0,0 +1,11 @@
+﻿Public Enum Wochentag As Byte
+
+    Montag = 10
+    Dienstag
+    Mittwoch
+    Donnerstag = 3
+    Freitag
+    Samstag
+    Sonntag
+
+End Enum
