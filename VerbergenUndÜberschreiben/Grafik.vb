4f5a60e643ba9f583a65ed6cf69e61bd736c2a9e
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-09-08 11:15:44 +0200

    Verberen & Überschreiben

---
+++ VerbergenUndÜberschreiben/Grafik.vb
@@ -0,0 +1,21 @@
+﻿Public Class Grafik
+
+    Private _farbe As String
+    Public Property Farbe() As String
+        Get
+            Return _farbe
+        End Get
+        Set(ByVal value As String)
+            _farbe = value
+        End Set
+    End Property
+
+    Public Overridable Sub Zeichnen()       'overridable -> Diese Methode darf überschrieben werden!
+        Console.WriteLine($"Zeichne Grafik in der Farbe {Farbe}.")
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return $"Eine Grafik in der Farbe {Farbe}."
+    End Function
+
+End Class
---
+++ VerbergenUndÜberschreiben/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim g = New Grafik()
+        g.Farbe = "Rot"
+        g.Zeichnen()
+
+        Dim k = New Kreis()
+        k.Farbe = "Blau"
+        k.Radius = 2.12
+        k.Zeichnen()
+
+        Dim l = New Linie()
+        l.Farbe = "Grün"
+        l.Länge = 7
+        l.Zeichnen()
+
+        Console.WriteLine()
+        VerwendeGrafik(g)
+        VerwendeGrafik(k)
+        VerwendeGrafik(l)
+
+        Console.WriteLine(g)
+        Console.ReadLine()
+    End Sub
+
+    Private Sub VerwendeGrafik(g As Grafik)
+        g.Zeichnen()
+    End Sub
+
+End Module
